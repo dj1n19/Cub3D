c88bbf668a4b140d8fc270763fbf90c5dd5cf46e
Bruno Genie <bgenie@fu-r8-p5.s19.be>
2023-09-28 15:24:39 +0200

    check map file extension

--- srcs/main.c
+++ srcs/main.c
@@ -3,14 +3,15 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgenie <bgenie@student.s19.be>             +#+  +:+       +#+        */
+/*   By: bgenie <bgenie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 06:46:58 by mebourge          #+#    #+#             */
-/*   Updated: 2023/09/26 23:41:37 by bgenie           ###   ########.fr       */
+/*   Updated: 2023/09/28 15:13:15 by bgenie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/cub3d.h"
+#include <string.h>
 
 int	ft_exit(void *data)
 {
@@ -24,9 +25,13 @@ int	main(int argc, char **argv)
 {
 	int		fd;
 	t_map	map;
+	char	*ext;
 
 	if (argc != 2)
 		ft_error(ARGUMENT_ERROR);
+	ext = ft_strrchr(argv[1], '.');
+	if (ft_strnstr(ext, ".cub", 5) == NULL || *(ext + 4) != 0)
+		ft_error("Bad file format");
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 1)
 		ft_error(FILE_DESRIPTOR_ERROR);
--- srcs_bonus/main_bonus.c
+++ srcs_bonus/main_bonus.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main_bonus.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: bgenie <bgenie@student.s19.be>             +#+  +:+       +#+        */
+/*   By: bgenie <bgenie@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/12 06:46:58 by mebourge          #+#    #+#             */
-/*   Updated: 2023/09/26 23:49:34 by bgenie           ###   ########.fr       */
+/*   Updated: 2023/09/28 15:14:46 by bgenie           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,9 +24,13 @@ int	main(int argc, char **argv)
 {
 	int		fd;
 	t_map	map;
+	char	*ext;
 
 	if (argc != 2)
 		ft_error(ARGUMENT_ERROR);
+	ext = ft_strrchr(argv[1], '.');
+	if (ft_strnstr(ext, ".cub", 5) == NULL || *(ext + 4) != 0)
+		ft_error("Bad file format");
 	fd = open(argv[1], O_RDONLY);
 	if (fd < 1)
 		ft_error(FILE_DESRIPTOR_ERROR);
